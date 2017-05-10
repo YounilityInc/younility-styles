29a43642774327faf5208f74d5ff3a8a714ba24d
Desiree <desiree.melcer@younility.com>
2017-05-10 16:23:49 -0400

    Da rubocop

--- lib/younility_styles/engine.rb
+++ lib/younility_styles/engine.rb
@@ -2,6 +2,7 @@ require 'autoprefixer-rails'
 
 module YounilityStyles
   # Add our assets to Rails using an engine
+
   class Engine < ::Rails::Engine
     initializer 'younility_styles.assets' do |app|
       %w(stylesheets javascripts fonts).each do |sub|
