67f895a7e8046fc1bd614e251e18e5b7e7b43fcb
Desiree Melcer <desiree.melcer@younility.com>
2017-12-15 20:16:03 +0000

    Put dependant locals and images within the gem

--- lib/younility_styles/engine.rb
+++ lib/younility_styles/engine.rb
@@ -4,7 +4,7 @@ module YounilityStyles
   # Add our assets to Rails using an engine
   class Engine < ::Rails::Engine
     initializer 'younility_styles.assets' do |app|
-      %w(stylesheets javascripts fonts).each do |sub|
+      %w(stylesheets javascripts fonts images).each do |sub|
         app.config.assets.paths << root.join('assets', sub).to_s
       end
     end
